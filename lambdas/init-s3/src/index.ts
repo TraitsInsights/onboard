68b00a906969fe97546a35502ab3bad532015f92
lewisvatcky <lewis@traitsinsights.com>
2024-11-11 16:39:27 +1100

    feat: update

--- lambdas/init-s3/src/index.ts
+++ lambdas/init-s3/src/index.ts
@@ -3,15 +3,12 @@ import { generateHandler } from "@shared/util";
 import { InitS3 } from "./InitS3";
 
 export const handler = generateHandler(async (event: APIGatewayProxyEvent) => {
-  console.log("1");
   if (!event.body) {
     throw new Error("no body present");
   }
 
   const payload = JSON.parse(event.body);
 
-  console.log("2");
   const initS3 = new InitS3();
-  console.log("3");
   await initS3.invoke(payload);
 });
